5290c03b10cfe85a00488cc0e206f1ecf63fc3b7
Jonty Ward <jward0395@gmail.com>
2021-03-25 19:07:35 +0000

    affing from

--- Backend/controllers/parks.js
+++ Backend/controllers/parks.js
@@ -12,11 +12,7 @@ export const getAllParks = async (_req, res) => {
 export const getOnePark = async (req, res) => {
   try {
     const { id } = req.params
-<<<<<<< HEAD
-    const singlePark = await Park.findById(id)
-=======
     const singlePark = await Park.findById(id).populate('comments.owner').populate('recommendations.owner')
->>>>>>> development
     if (!singlePark) {
       throw new Error()
     }
