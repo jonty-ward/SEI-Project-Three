93f89c0e23f1b0e98c31f1023e86fb1e87eedc2a
Hannah Tinacci <hannahtinacci@gmail.com>
2021-03-25 20:22:09 +0000

    register and login forms added to front end

--- client/src/auth/Login.js
+++ client/src/auth/Login.js
@@ -1,9 +1,66 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
+import { useHistory } from 'react-router-dom'
 
 const Login = () => {
+
+  const [formData, setFormData] = useState({
+    email: '',
+    password: ''
+  })
+
+  const history = useHistory()
+
+  const handleChange = (event) => {
+    const newFormData = { ...formData, [event.target.name]: event.target.value }
+    setFormData(newFormData)
+  }
+  
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    const response = await axios.post('/api/login', formData)
+    window.localStorage.setItem('token', response.data.token)
+    history.push('/parks')
+  }
+
   return (
-    <div>
-      <h1>Login Page</h1>
+    <div className="background-color">
+      <section className="section">
+        <div className="container">
+          <div className="columns">
+            <form onSubmit={handleSubmit} className="box column is-half is-offset-one-quarter" >
+              <div className="field">
+                <label className="label">Email</label>
+                <div className="control">
+                  <input
+                    className="input"
+                    placeholder="Email"
+                    name="email"
+                    onChange={handleChange}
+                    value={formData.email}
+                  />
+                </div>
+              </div>
+              <div className="field">
+                <label className="label">Password</label>
+                <div className="control">
+                  <input
+                    className="input"
+                    type="password"
+                    placeholder="Password"
+                    name="password"
+                    onChange={handleChange}
+                    value={formData.password}
+                  />
+                </div>
+              </div>
+              <div className="field">
+                <button type="submit" className="button is-fullwidth">Login</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </section>
     </div>
   )
 }
