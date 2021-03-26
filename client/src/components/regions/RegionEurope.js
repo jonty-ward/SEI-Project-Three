8dd64d56d9d8601633e02bacb066270e09ea438d
michael_edwards1994@hotmail.com <michael_edwards1994@hotmail.com>
2021-03-26 14:49:31 +0000

    basic pathing thorugh regions

---
+++ client/src/components/regions/RegionEurope.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import ParkIndex from '../parks/ParkIndex'
+
+const RegionEurope = () => {
+
+
+
+
+
+
+
+  return (
+    <>
+      <div>
+      Europe
+      </div>
+      <ParkIndex />
+    </>
+  )
+}
+
+export default RegionEurope 
